1a759acfc65318e90e88551ebfcbb3aa884b80d9
CyrusPar <cmpar@up.edu.ph>
2024-09-22 20:10:17 +0800

    feat: Css implementation on buildclientprofile

--- frontend/src/Pages/BuildClient.js
+++ frontend/src/Pages/BuildClient.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useOutletContext, useNavigate } from "react-router-dom";
 
 import rectangleImage from '../assets/Rectangle 3.png'; 
